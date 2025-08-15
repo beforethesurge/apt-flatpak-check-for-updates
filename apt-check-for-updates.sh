dcb028bd2a45727e4b864d409056520e82b21424
beforethesurge <beforethesurge@gmail.com>
2025-08-15 01:06:28 +0000

    Added or operation and hopefully fixed package listing

--- apt-check-for-updates.sh
+++ apt-check-for-updates.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 echo "Checking for updates..."
-sudo apt update
+sudo apt-get update
 
-packages_to_update=$(apt list --upgradeable | grep -o -E '\w+/\w+')
+packages_to_update=$(apt list --upgradeable | grep "*-*")
 if [[ -n "$packages_to_update" ]]; then
     echo "The following updates are available:"
     echo "$packages_to_update"
-    read -p "Apply? (y/n): " user_apply_updates
+    read -p "Apply? (Y/n): " user_apply_updates
     user_apply_updates=$(echo "$user_apply_updates" | tr "[:upper:]" "[:lower:]")
-    if [[ "$user_apply_updates" == "y" ]]; then
+    if [[ "$user_apply_updates" == "y" || "$user_apply_updates" == "" ]]; then
         echo "Applying updates..."
-        sudo apt full-upgrade -y
+        sudo apt-get upgrade -y
         echo "Updates applied successfully!"
     else
         echo "Updates skipped"
@@ -21,14 +21,14 @@ else
 fi
 
 echo "Checking for packages no longer being used..."
-packages_to_remove_check=$(apt autoremove --dry-run | grep -o -E '\w+/\w+')
+packages_to_remove_check=$(apt autoremove --dry-run | grep "*-*")
 
 if [[ -n "$packages_to_remove_check" ]]; then
-    read -p "Packages found. Remove them? (y/n): " user_remove_packages
+    read -p "Packages found. Remove them? (Y/n): " user_remove_packages
     user_remove_packages=$(echo "$user_remove_packages" | tr "[:upper:]" "[:lower:]")
-    if [[ "$user_remove_packages" == "y" ]]; then
+    if [[ "$user_remove_packages" == "y" || "$user_remove_packages" == "" ]]; then
         echo "Removing packages..."
-        sudo apt autoremove -y
+        sudo apt-get autoremove -y
         echo "Packages removed successfully!"
     else
         echo "Packages skipped"
