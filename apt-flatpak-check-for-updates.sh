bf05c9b06e1b430d25e89ea77f0906268f68caa7
beforethesurge <beforethesurge@gmail.com>
2025-09-06 04:24:35 +0000

    Fixed an issue Flatpak responses

--- apt-flatpak-check-for-updates.sh
+++ apt-flatpak-check-for-updates.sh
@@ -33,15 +33,12 @@ fi
 echo
 if command -v flatpak >/dev/null 2>&1; then
     echo "Flatpak is installed"
-    flatpak_check_for_updates=$(flatpak update --appstream -y --noninteractive 2>&1 | grep -E 'Updates available|Nothing to do')
-    if echo "$flatpak_check_for_updates" | grep -q "Updates available"; then
-        echo "📦 Flatpak updates found. Downloading and installing updates..."
-        flatpak update -y
-    elif echo "$flatpak_check_for_updates" | grep -q "Nothing to do"; then
+    flatpak_check_for_updates=$(flatpak update --noninteractive 2>&1)
+    if echo "$flatpak_check_for_updates" | grep -qE "^Nothing to do"; then
         echo "✅ No Flatpak updates found"
     else
-        echo "⚠️ Unexpected output while checking for updates:"
-        echo "$updates"
+        echo "📦 Flatpak updates found. Downloading and installing updates..."
+        flatpak update -y
     fi
 else
     echo "❌ Flatpak is not installed; Skipping"
